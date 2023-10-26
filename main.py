93a51eb1540951e41b1e4646ac7372dfcbc3521f
Dhruv Aron <dhruv.aron@gmail.com>
2023-10-26 00:55:11 -0700

    spaghetti plot basically done (slight sorting issue), daily plots not showing events for some reason though

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ def main():
    df = import_directory("datasets")
    
    event1 = pd.DataFrame.from_records([{'id': "NathanielBarrow", time(): '2023-08-08 7:45:35', 'before': 2, 'after': 3, 'description': 'testing1'}])
-   event2 = pd.DataFrame.from_records([{'id': "NathanielBarrow", time(): '2023-08-13 2:55:35', 'before': 10, 'after': 10, 'description': 'plot test'}])
-   #event2 = pd.DataFrame.from_records([{'id': "ElizaSutherland2/23/68", time(): '2023-07-03 3:15:17', 'before': 6, 'after': 9, 'description': 'testing2'}])
-   #event3 = pd.DataFrame.from_records([{'id': "PenelopeFitzroy1/5/52", time(): '2023-06-24 0:26:10', 'before': 3, 'after': 1, 'description': 'testing3'}])
-   events = pd.concat([event1, event2])
+   event2 = pd.DataFrame.from_records([{'id': "NathanielBarrow", time(): '2023-08-13 2:55:35', 'before': 10, 'after': 10, 'description': 'testing2'}])
+   event3 = pd.DataFrame.from_records([{'id': "ElizaSutherland", time(): '2023-07-03 3:15:17', 'before': 6, 'after': 9, 'description': 'testing3'}])
+   event4 = pd.DataFrame.from_records([{'id': "PenelopeFitzroy", time(): '2023-06-24 0:26:10', 'before': 3, 'after': 1, 'description': 'testing4'}])
+   events = pd.concat([event1, event2, event3, event4])
    
    event_df = retrieve_event_data(df, events)
    #create_event_features(df, events)
-
-   daily_plot_all(df)
+   #daily_plot(df, "NathanielBarrow", save=False)
+   #daily_plot_all(df, events)
+   spaghetti_plot_all(df)
 
 if __name__ == "__main__":
    main()
--- plots.py
+++ plots.py
@@ -7,18 +7,70 @@ import matplotlib.pyplot as plt
 Plots and saves all of the patients data in separate graphs
 @param df   a Multiindexed DataFrame grouped by 'id' and containing DateTime and Glucose columns
 """
-def daily_plot_all(df):
+def daily_plot_all(df, events, save=False):
    sns.set_theme()
    for id, data in df.groupby('id'):
-      plot = sns.relplot(data=data, kind="line", x=time(), y=glucose())
+      plot = sns.lineplot(data=data, x=time(), y=glucose())
 
-      plot.savefig("./plots/" + str(id) + '.png')
+      events.set_index('id', inplace=True)
+
+      event_times = pd.to_datetime(events.loc[id][time()])
+      print(event_times)
+      plt.vlines(x = event_times, ymin = 0, ymax = 1, colors='red', linestyles='dashed')
+
+      #for row in events.loc[id]:
+         #plot.axvline(row[time()], color='r')
+
+      events.reset_index(inplace=True)
+
+      plt.show()
+
+      if save:
+         plot.savefig("./plots/" + str(id) + 'Daily.png')
 
 """
 Plots and saves only the given patient's data
-@param df   a default DataFrame with only DateTime and Glucose columns
+@param df   a Multiindexed DataFrame grouped by 'id' and containing DateTime and Glucose columns
+@param id   the id of the patient whose data is graphed
 """
-def daily_plot(df):
-   plot = sns.relplot(data=df, kind="line", x=time(), y=glucose())
+def daily_plot(df, id, save=False):
+   data = df.loc[id]
+   plot = sns.lineplot(data=data, x=time(), y=glucose())
 
-   plot.savefig("./plots/" + str(id) + '.png')
+   plot.axvline(pd.to_datetime("2023-08-08 7:45:35"))
+
+   plt.show()
+
+   if save:
+      plot.savefig("./plots/" + str(id) + 'Daily.png')
+
+"""
+"""
+def spaghetti_plot_all(df):
+   sns.set_theme()
+   for id, data in df.groupby('id'):
+      spaghetti_plot(data, id)
+
+"""
+"""
+def spaghetti_plot(df, id):
+   data = df.loc[id]
+   data.reset_index(inplace=True)
+   
+   datetimes = pd.to_datetime(data[time()])
+   date = datetimes.dt.date
+   times = datetimes.dt.time
+
+   time_data = pd.DataFrame()
+   time_data['Day'] = date
+   time_data['Time'] = times.astype(str)
+   time_data[glucose()] = data[glucose()]
+
+   time_data.set_index('Day', inplace=True)
+   time_data.sort_values(by=['Time', 'Day'], ascending=[True, True], inplace=True)
+
+   print(time_data)
+
+   plot = sns.relplot(data=time_data, kind="line", x='Time', y=glucose(), hue='Day')
+
+   plot.savefig("./plots/" + str(id) + 'Spaghetti.png')
