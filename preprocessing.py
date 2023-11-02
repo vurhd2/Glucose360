d3e0566f6144e2a4c3445deb434dd9f450201164
Dhruv Aron <dhruv.aron@gmail.com>
2023-11-02 14:34:41 -0700

    fixed interpolation method, slight issue with function automatically removing rows with NaNs at the start though

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import glob
 import pandas as pd
+import numpy as np
 
 """
 Returns a Multiindexed Pandas DataFrame containing all of the csv data found in the directory at the given path
@@ -32,7 +33,7 @@ def import_directory(path, glucose_col="Glucose Value (mg/dL)", time_col="Timest
 Returns a pre-processed Pandas DataFrame containing the timestamp and glucose data for the csv file at the given path
 @param path    the path of the csv file to be pre-processed and read into a Pandas Dataframe
 """
-def import_data(path, interval):
+def import_data(path, interval=5):
    df = pd.read_csv(path)
 
    #id = df["Patient Info"].iloc[0] + df["Patient Info"].iloc[1] + df["Patient Info"].iloc[2]
@@ -67,11 +68,44 @@ def resample_data(df, minutes=5):
    df = df.set_index(time_name)
 
    resampled_df = df.resample(interval, origin='start').mean()
-   resampled_df.interpolate('linear', inplace=True) 
+   resampled_df = interpolate_data(resampled_df) 
 
    resampled_df.reset_index(inplace=True)
 
    return resampled_df
+
+"""
+Only interpolates NaN glucose values for time gaps that are less than the given number of minutes
+@param df         a DataFrame with only two columns, DateTime and Glucose Value
+@param max_gap    the maximum minute length of gaps that should be interpolated
+"""
+def interpolate_data(df, max_gap=30):
+   df.reset_index(inplace=True)
+
+   gaps = []
+   index_range = {}
+   for i in range(len(df) - 1):
+      is_nan = lambda x : np.isnan(df.iloc[x][glucose()])
+      timestamp = lambda x : df.iloc[x][time()]
+      length = len(index_range)
+
+      # start of time gap needing interpolation
+      if length == 0 and not is_nan(i) and is_nan(i+1):
+         index_range['start'] = timestamp(i)
+
+      # end of time gap needing interpolation
+      elif length == 1 and is_nan(i) and not is_nan(i+1):
+         index_range['end'] = timestamp(i+1)
+         if index_range['end'] - index_range['start'] <= pd.Timedelta(minutes=max_gap):
+            gaps.append(index_range)
+         index_range = {}
+   
+   df.set_index(time(), inplace=True)
+   # interpolate only the necessary gaps of time
+   for gap in gaps:
+      df.loc[gap['start']:gap['end']] = df.loc[gap['start']:gap['end']].copy().interpolate('linear', limit_area='inside')
+   
+   return df
 
 def glucose():
    return glucose_name
