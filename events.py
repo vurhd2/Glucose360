c1c6007644e8c2242bdebda4c545991e371be8ce
Dhruv Aron <dhruv.aron@gmail.com>
2024-02-07 15:50:27 -0800

    edited episodes to check 15 min after to make sure the episode has ended

--- events.py
+++ events.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 from features import create_features
 import configparser
+import math
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -8,48 +10,77 @@ GLUCOSE = config['variables']['glucose']
 TIME = config['variables']['time']
 INTERVAL = config['variables'].getint('interval')
 
+def episodes_helper(
+   df: pd.DataFrame, 
+   id: str, 
+   type: str, 
+   threshold: int, 
+   level: int, 
+   min_length: int, 
+   end_length: int
+   ):
+   timegap = lambda timedelta: timedelta.total_seconds() / 60
+   episodes = pd.DataFrame()
+
+   data = df.copy(); data.reset_index(drop=True, inplace=True)
+   episode_df = df[(df[GLUCOSE] <= threshold)].copy() if type == "hypo" else df[df[GLUCOSE] >= threshold].copy()
+   episode_df.reset_index(drop=True, inplace=True)
+   episode_df["gap"] = episode_df[TIME].diff().apply(timegap)
+
+   edges = episode_df.index[episode_df["gap"] != INTERVAL].to_list()
+   edges.append(-1)
+
+   get = lambda loc, col: episode_df.iloc[loc][col]
+   index = 0
+   while index < len(edges) - 1:
+      offset = 0 if (index == len(edges) - 2) else 1
+      end_i = edges[index + 1] - offset # index of the end of the episode (inclusive! - that's what the offset is for)
+      start_i = edges[index] # index of the start of the episode
+      start_time = get(start_i, TIME)
+      end_time = get(end_i, TIME)
+      episode_length = timegap(end_time - start_time)
+
+      if episode_length >= min_length: # check if episode lasts longer than 15 min
+         if offset != 0: # not the very last episode
+            end_counts = math.ceil(end_length / INTERVAL)
+            
+            end_index = data.index[data[TIME] == end_time].to_list()[0]
+            end_data = data.iloc[end_index + 1 : end_index + 1 + end_counts][GLUCOSE]
+            outside_threshold = np.where(end_data >= threshold, True, False) if type == "hypo" else np.where(end_data <= threshold, True, False)
+            if False in outside_threshold: # check if episode ends within 15 min
+               edges.pop(index + 2)
+               edges.pop(index + 1) # this episode does not end within 15 min, so combine this episode with the next
+               continue
+
+         description = f"{type}glycemic episode of level {level} occurring from {start_time} to {end_time}"
+         event = pd.DataFrame.from_records([{"id": id, TIME: start_time, "before": 0, "after": episode_length, 
+                                             "type": f"{type} level {level} episode", "description": description}])
+         episodes = pd.concat([episodes, event]) 
+      
+      index += 1
+
+   return episodes
+
 def get_episodes(
     df: pd.DataFrame,
     hypo_lvl1: int = 70,
     hypo_lvl2: int = 54,
     hyper_lvl1: int = 180,
     hyper_lvl2: int = 250,
-    min_length: int = 15
+    min_length: int = 15,
+    end_length: int = 15
 ) -> pd.DataFrame:
-   episodes = pd.DataFrame()
+   output = pd.DataFrame()
    for id, data in df.groupby('id'):
-      timegap = lambda timedelta: timedelta.total_seconds() / 60
+      episodes = pd.concat([episodes_helper(data, id, "hyper", hyper_lvl1, 1, min_length, end_length),
+                            episodes_helper(data, id, "hyper", hyper_lvl2, 2, min_length, end_length),
+                            episodes_helper(data, id, "hypo", hypo_lvl1, 1, min_length, end_length),
+                            episodes_helper(data, id, "hypo", hypo_lvl2, 2, min_length, end_length)])
+      
+      episodes.sort_values(by=[TIME], inplace=True)
+      output = pd.concat([output, episodes])
 
-      episode_df = data[(data[GLUCOSE] <= hypo_lvl1) | (data[GLUCOSE] >= hyper_lvl1)].copy()
-      print(episode_df)
-      episode_df.reset_index(drop=True, inplace=True)
-      episode_df["gap"] = episode_df[TIME].diff().apply(timegap)
-
-      edges = episode_df.index[episode_df["gap"] != INTERVAL].to_list()
-      edges.insert(0,0); edges.append(-1)
-
-      get = lambda loc, col: episode_df.iloc[loc][col]
-      for index in range(1, len(edges)):
-         offset = 0 if (index == len(edges) - 1) else 1
-         end_i = edges[index] - offset # index of the end of the episode (inclusive! - that's what the offset is for)
-         start_i = edges[index - 1] # index of the start of the episode
-         episode_length = timegap(get(end_i, TIME) - get(start_i, TIME))
-
-         if episode_length >= min_length:
-            type = "hyper" if (get(start_i, GLUCOSE) >= hyper_lvl1) else "hypo"
-
-            level = 1
-            current_episode = episode_df.iloc[start_i:end_i+offset]
-            if ((type == "hyper" and (current_episode[current_episode[GLUCOSE] >= hyper_lvl2].shape[0] > (min_length / INTERVAL))) or 
-                (type == "hypo" and (current_episode[current_episode[GLUCOSE] <= hypo_lvl2].shape[0] > (min_length / INTERVAL)))):
-               level = 2
-
-            description = f"{type}glycemic episode of level {level} occurring from {get(start_i, TIME)} to {get(end_i, TIME)}"
-            event = pd.DataFrame.from_records([{"id": id, TIME: get(start_i, TIME), "before": 0, "after": episode_length, 
-                                               "type": f"{type} level {level} episode", "description": description}])
-            episodes = pd.concat([episodes, event]) 
-
-   return episodes
+   return output 
 
 def excursions(df: pd.DataFrame) -> pd.Series:
     """
@@ -88,6 +119,9 @@ def excursions(df: pd.DataFrame) -> pd.Series:
             excursions.append(copy.idxmin())
 
     return pd.Series(excursions)
+
+def event_summary(events: pd.DataFrame, type: str = "type") -> pd.Series:
+    return events[type].value_counts()
 
 def retrieve_event_data(
     df: pd.DataFrame,
@@ -130,19 +164,6 @@ def retrieve_event_data(
 
     return event_data
 
-
-"""
-Returns a multiindexed Pandas DataFrame containing metrics for the patient data during their respective 'events'
-@param df      a multiindexed Pandas DataFrame containing all the relevant patient data
-@param events  a single indexed Pandas DataFrame, with each row specifying a single event in the form of
-               an id, a datetime, # of hours before the datetime to include, # of hours after to include, and a desc
-@param before  name of the column specifying the amount of hours before to include
-@param after   name of the column specifying the amount of hours after to include
-@param type    name of the column specifying the 'type' of event (like 'meal', 'exercise', etc.)
-@param desc    name of the column describing this particular event 
-"""
-
-
 def create_event_features(
     df: pd.DataFrame,
     events: pd.DataFrame,
@@ -151,5 +172,15 @@ def create_event_features(
     type: str = "type",
     desc: str = "description",
 ) -> pd.DataFrame:
+    """
+    Returns a multiindexed Pandas DataFrame containing metrics for the patient data during their respective 'events'
+    @param df      a multiindexed Pandas DataFrame containing all the relevant patient data
+    @param events  a single indexed Pandas DataFrame, with each row specifying a single event in the form of
+                   an id, a datetime, # of hours before the datetime to include, # of hours after to include, and a desc
+    @param before  name of the column specifying the amount of hours before to include
+    @param after   name of the column specifying the amount of hours after to include
+    @param type    name of the column specifying the 'type' of event (like 'meal', 'exercise', etc.)
+    @param desc    name of the column describing this particular event 
+    """
     event_data = retrieve_event_data(df, events, before, after, type, desc)
     return create_features(event_data, events=True)
